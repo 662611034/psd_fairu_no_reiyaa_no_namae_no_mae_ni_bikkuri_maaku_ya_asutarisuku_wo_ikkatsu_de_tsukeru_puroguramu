03c73f5a082edaa912b162cf2c7c3cbeb8b4f204
Istallia <55042065+istallia@users.noreply.github.com>
2020-12-07 21:32:45 +0900

    レジストリの登録先が間違っていたのを修正

--- regist.bat
+++ regist.bat
@@ -3,11 +3,11 @@ chcp 932
 rem 「PSDファイルのレイヤーを編集」をレジストリに登録するバッチファイル
 
 setlocal
-set APP_PATH=%~dp0psd_fairu_no_reiyaa_no_namae_no_mae_ni_bikkuri_maaku_ya_asutarisuku_wo_ikkatsu_de_tsukeru_puroguramu_201023.exe
+set APP_PATH=%~dp0psd_fairu_no_reiyaa_no_namae_no_mae_ni_bikkuri_maaku_ya_asutarisuku_wo_ikkatsu_de_tsukeru_puroguramu_201207.exe
 set COMMAND1=\"%APP_PATH%\"
 set COMMAND2=\"%%1\"
 set COMMAND3=%COMMAND1% %COMMAND2%
 echo %COMMAND1% %COMMAND2%
 
-rem reg add "HKEY_CURRENT_USER\Software\Classes\SystemFileAssociations\.aup\Shell\コモンズ素材の一覧を生成する" /v "Icon" /t REG_SZ /d "%APP_PATH%" /f
-reg add "HKEY_CURRENT_USER\Software\Classes\SystemFileAssociations\.aup\Shell\コモンズ素材の一覧を生成する\command" /ve /t REG_SZ /d "%COMMAND3%" /f
+rem reg add "HKEY_CURRENT_USER\Software\Classes\SystemFileAssociations\.psd\Shell\PSDファイルのレイヤーを編集" /v "Icon" /t REG_SZ /d "%APP_PATH%" /f
+reg add "HKEY_CURRENT_USER\Software\Classes\SystemFileAssociations\.psd\Shell\PSDファイルのレイヤーを編集\command" /ve /t REG_SZ /d "%COMMAND3%" /f
